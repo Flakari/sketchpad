935f392944d20c3b5b740509a6acc756a3f9e518
Nate Dimock <natedimock@yahoo.com>
2018-01-04 10:17:09 -0600

    Began refactor of code from jQuery to vanilla JS, title pixel and sketchpad pixel functionality working

--- scripts/script.js
+++ scripts/script.js
@@ -4,27 +4,30 @@ console.log(16 * 16);
 const titleDensity = 36;
 let pixelDensity = 16;
 let pixelPrompt = 1;
+let sketchpad = document.querySelector('#sketchpad');
+let title = document.querySelector('#title');
 
 
-$(document).ready(function() {
-  //Sketch pad creation function
-  (function pixelChange() {
-    for (let i = 0; i < (pixelDensity * pixelDensity); i++) {
-      $('#sketchpad').append("<div class='sketchpad-pixel'></div>");
-    }
+(function pixelChange() {
+  for (let i = 0; i < (pixelDensity * pixelDensity); i++) {
+    let sketchpadPixel = document.createElement('div');
+    sketchpadPixel.classList.add('sketchpad-pixel');
+    sketchpad.appendChild(sketchpadPixel);
 
-    $('.sketchpad-pixel').css({"width": "calc(100% / " + pixelDensity + ")", "height": "calc(100% / " + pixelDensity + ")"});
-  }());
+    sketchpadPixel.style.cssText = 'width: calc(100% / ' + pixelDensity + '); height: calc(100% / ' + pixelDensity + ')';
+  }
+}());
 
-  //Title container creation function
-  (function titleChange() {
-    for (let i = 0; i < (titleDensity * 5); i++) {
-      $('#title').append("<div class='title-pixel'></div>");
-    }
-
-    $('.title-pixel').css({"width": "16px", "height": "16px"});
-  }());
-
+//Title container creation function
+(function titleChange() {
+  for (let i = 0; i < (titleDensity * 5); i++) {
+    let titlePixel = document.createElement('div');
+    titlePixel.classList.add('title-pixel');
+    title.appendChild(titlePixel);
+    titlePixel.style.cssText = 'width: 16px; height: 16px';
+  }
+}());
+/*
   //Title creation function
   (function writeTitle() {
     let arr = [0,1,2,4,6,8,9,10,12,13,14,16,17,18,20,22,25,26,27,29,30,31,33,34,36,40,42,44,49,52,56,58,61,63,65,67,69,71,72,73,74,76,77,80,81,82,85,88,92,93,94,97,98,99,101,102,103,105,107,110,112,114,116,121,124,128,130,133,137,139,141,143,144,145,146,148,150,152,153,154,157,160,161,162,164,166,169,173,175,177,178];
@@ -142,5 +145,4 @@ $(document).ready(function() {
   $('#clear').click(function() {
     $('.sketchpad-pixel').css({'background-color': '#e6e6e6'});
     pixelColor = greyPixel();
-  })
-});
+  })*/
